bdd9f7248e9bfb7ee84a68d303df8606340ef01c
Xinxin Yang <xinxin.yang@thuenen.de>
2024-10-24 14:40:40 +0200

    update deploy

--- docs/.vuepress/.temp/pages/sub1/foo.html.js
+++ docs/.vuepress/.temp/pages/sub1/foo.html.js
@@ -1,3 +1,3 @@
 import comp from "C:/Users/yang_x/2024/vuepress-starter/docs/.vuepress/.temp/pages/sub1/foo.html.vue"
-const data = JSON.parse("{\"path\":\"/sub1/foo.html\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Foo in sub1\",\"slug\":\"foo-in-sub1\",\"link\":\"#foo-in-sub1\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"sub1/foo.md\"}")
+const data = JSON.parse("{\"path\":\"/sub1/foo.html\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Foo in sub1\",\"slug\":\"foo-in-sub1\",\"link\":\"#foo-in-sub1\",\"children\":[]}],\"git\":{\"updatedTime\":1729773561000,\"contributors\":[{\"name\":\"Xinxin Yang\",\"email\":\"xinxin.yang@thuenen.de\",\"commits\":1}]},\"filePathRelative\":\"sub1/foo.md\"}")
 export { comp, data }
